699ee221d09656339e9b49ce83210471e1701dce
Adil Hassan A K <adilhassan9995@gmail.com>
2026-08-09 21:43:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: {
-    base: "/siih2.0/",
-  },
-
   tanstackStart: {
     server: {
       entry: "server",
