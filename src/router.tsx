84e0857a60cc46004ec1bb519763cfdb793c5a0b
Adil Hassan A K <adilhassan9995@gmail.com>
2026-08-09 21:37:19 +0530

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -7,6 +7,7 @@ export const getRouter = () => {
 
   const router = createRouter({
     routeTree,
+    basepath: "/siih2.0",
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
